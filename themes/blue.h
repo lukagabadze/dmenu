af7e465ae95fb05a3df3ce800aa7f3af4b786ed6
lukagabadze <gabadzeluka2003@gmail.com>
2022-07-31 16:36:30 +0400

    root commit

---
+++ themes/blue.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*               fg         bg       */
+	[SchemeNorm] = { "#bfd0e8", "#000000" },
+	[SchemeSel] =  { "#bfd0e8", "#1f9add" },
+	[SchemeOut] =  { "#bfd0e8", "#67A0D6" },
+};
