3dc8fc20a3e13da7713645bba531ed0e99320cb8
lukagabadze <gabadzeluka2003@gmail.com>
2022-10-11 01:51:31 +0400

    Green theme

---
+++ themes/green.h
@@ -0,0 +1,8 @@
+static const char *colors[SchemeLast][2] = {
+	/*               fg         bg       */
+	[SchemeNorm] = { "#bfd0e8", "#000000" },
+	[SchemeSel] =  { "#bfd0e8", "#35bf5c" },
+	[SchemeSelHighlight] = { "#000000", "#35bf5c" },
+	[SchemeNormHighlight] = { "#35bf5c", "#000000" },
+	[SchemeOut] =  { "#bfd0e8", "#67A0D6" },
+};
